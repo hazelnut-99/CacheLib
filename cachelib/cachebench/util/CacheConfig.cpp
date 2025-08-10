592c0653dfc164e29b8b8742cb167328b680316a
hongshu <hongshuyan99@outlook.com>
2025-08-10 10:37:01 -0400

    fix

--- cachelib/cachebench/util/CacheConfig.cpp
+++ cachelib/cachebench/util/CacheConfig.cpp
@@ -184,7 +184,7 @@ CacheConfig::CacheConfig(const folly::dynamic& configJson) {
   // if you added new fields to the configuration, update the JSONSetVal
   // to make them available for the json configs and increment the size
   // below
-  checkCorrectSize<CacheConfig, 1072>();
+  checkCorrectSize<CacheConfig, 1064>();
 
   if (numPools != poolSizes.size()) {
     throw std::invalid_argument(folly::sformat(
